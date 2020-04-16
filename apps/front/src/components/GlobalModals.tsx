3ae280a8b0124b41b60581f73c862c59cc3ca1bd
Łukasz Sentkiewicz <lukasz@sentkiewicz.pl>
2020-04-16 17:51:49 +0200

    wip

---
+++ apps/front/src/components/GlobalModals.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { LoginView } from 'src/features/login/components/LoginView';
+
+export function GlobalModals() {
+  return (
+    <>
+      <LoginView isModal />
+    </>
+  );
+}
