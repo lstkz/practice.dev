15b1e05ef32a35b26dc78e898559e3f92c336884
Łukasz Sentkiewicz <lukasz@sentkiewicz.pl>
2020-07-21 10:15:50 +0200

    ignore confirm email if already verified

--- apps/front/src/features/confirm/module.ts
+++ apps/front/src/features/confirm/module.ts
@@ -4,10 +4,13 @@ import { getRouteParams, createUrl } from 'src/common/url';
 import { api } from 'src/services/api';
 import { handleAppError } from 'src/common/helper';
 import { RouterActions } from 'typeless-router';
-import { GlobalActions } from '../global/interface';
+import { GlobalActions, getGlobalState } from '../global/interface';
 
 // --- Epic ---
 handle.epic().on(ConfirmActions.$mounted, () => {
+  if (getGlobalState().user?.isVerified) {
+    return RouterActions.push(createUrl({ name: 'challenges' }));
+  }
   const { code } = getRouteParams('confirm');
   return api.user_confirmEmail(code).pipe(
     Rx.map(auth => GlobalActions.auth(auth, true)),
